cc84279a6a3e53daf999049090f6a0f59dca52f2
kkreiju <138178348+kkreiju@users.noreply.github.com>
2023-12-08 00:46:08 +0800

    changed background and dino

--- ActualGame.java
+++ ActualGame.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -9,115 +10,123 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class ActualGame extends JFrame implements KeyListener{
-	
-	Icon imageCharacter = new ImageIcon("textures\\dino.png");
+public class ActualGame extends JFrame implements KeyListener {
+
+	Icon dinoImage = new ImageIcon("textures\\dinasourstand.png");
 	ImageIcon logo = new ImageIcon("textures\\icon.png");
 	JLabel dino = new JLabel();
 	Icon cactusImage = new ImageIcon("textures\\cactus.png");
 	JLabel cactus = new JLabel();
+	JLabel scoreDisplay = new JLabel();
 	JPanel panelCharacter = new JPanel();
-	ImageIcon background = new ImageIcon();
+	//ImageIcon background = new ImageIcon();
 	Timer jumpTimer;
 	Timer cactusTimer;
-	
+	Timer scoreTimer;
+
+	boolean playing = false;
 	boolean goingUp = true;
-	
-	ActualGame(){
-		dino.setIcon(imageCharacter);
-		dino.setBounds(-20,105,360,360);
-		cactus.setIcon(cactusImage);
-		cactus.setBounds(700 ,360, 50, 50);
+	int score = 0;
+
+	ActualGame() {
+		// general options
 		this.setTitle("Dino Jump!");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
-		this.setSize(850,450);
+		this.setSize(850, 450);
 		this.setLayout(null);
 		this.setLocationRelativeTo(null);
-		this.setContentPane(new JLabel(new ImageIcon("textures\\background.jpg")));
+
+		// score
+		scoreDisplay.setBounds(750, 25, 70, 20);
+		scoreDisplay.setForeground(Color.WHITE);
+		// dino
+		dino.setIcon(dinoImage);
+		dino.setBounds(35, 110, 360, 360);
+		// cactus
+		cactus.setIcon(cactusImage);
+		cactus.setBounds(700, 360, 50, 50); // can be randomized
+		// cactus.setBorder(new LineBorder(Color.BLACK)); //border for collision
+		// purposes
+
+		// background
+		this.setContentPane(new JLabel(new ImageIcon("textures\\backgroundFin.png")));
 		this.setIconImage(logo.getImage());
 		this.addKeyListener(this);
 		this.add(dino);
+		scoreDisplay.setText("Score: 0");
+		this.add(scoreDisplay);
 		this.add(cactus);
 		this.setVisible(true);
-		
+
 		jumpTimer = new Timer(10, new jumpTimerListener());
 		cactusTimer = new Timer(10, new cactusListener());
-		cactusTimer.start();
+		scoreTimer = new Timer(100, new scoreTimerListener());
 	}
+
 	public void keyTyped(KeyEvent e) {
-		switch(e.getKeyChar()) {
-		
-		case 'a':
-				dino.setLocation(dino.getX()-10,dino.getY());
-				
-			break;
-		case 'w':
-				dino.setLocation(dino.getX(),dino.getY()-10);
-			break;
-		case 's':
-				dino.setLocation(dino.getX(),dino.getY()+10);
-			break;
-		case 'd':
-				dino.setLocation(dino.getX()+10,dino.getY());
-			break;
+		switch (e.getKeyChar()) {
 		case ' ':
+			if (!playing) {
+				playing = true;
+				cactusTimer.start();
+				scoreTimer.start();
+			} else {
+				jumpTimer.start();
+			}
+			break;
+		}
+
+	}
+
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case 38:
 			jumpTimer.start();
 			break;
-		}
-		
-	}
-	public void keyPressed(KeyEvent e) {
-		switch(e.getKeyCode()) {
-		
-		case 37:
-				dino.setLocation(dino.getX()-10,dino.getY());
-			break;
-		case 38:
-				dino.setLocation(dino.getX(),dino.getY()-10);
-			break;
 		case 40:
-				dino.setLocation(dino.getX(),dino.getY()+10);
-			break;
-		case 39:
-				dino.setLocation(dino.getX()+10,dino.getY());
+			System.out.println("Down");
 			break;
 		}
 	}
-		
+
 	public void keyReleased(KeyEvent e) {
-	System.out.print(e.getKeyChar());
-	System.out.print(e.getKeyCode());
 	}
-	
-	private class jumpTimerListener implements ActionListener{
+
+	private class jumpTimerListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if(goingUp) {
-				dino.setLocation(dino.getX(),dino.getY() - 5);
+			if (goingUp) {
+				dino.setLocation(dino.getX(), dino.getY() - 5);
+			} else {
+				dino.setLocation(dino.getX(), dino.getY() + 10);
 			}
-			else {
-				dino.setLocation(dino.getX(), dino.getY() + 15);
-			}
-			if(dino.getY() <= -105) {
+			if (dino.getY() <= -50) {
 				goingUp = false;
-			}
-			else if(dino.getY() >= 105){
-				dino.setLocation(dino.getX(), 105);
+			} else if (dino.getY() >= 110) {
+				dino.setLocation(dino.getX(), 110);
 				goingUp = true;
 				jumpTimer.stop();
 			}
 		}
 	}
-	
-	private class cactusListener implements ActionListener{
+
+	private class cactusListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			cactus.setLocation(cactus.getX() - 5, cactus.getY());
-			if(cactus.getX() < 0) {
+			if (cactus.getX() < 0) {
 				cactus.setLocation(700, cactus.getY());
 			}
 		}
 	}
 	
+	private class scoreTimerListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			score++;
+			scoreDisplay.setText("Score: " + score);
+		}
+	}
+
 }
